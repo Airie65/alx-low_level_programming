8f0611a18238af349ac539486797a14b7b881d64
AugustaOrji <augustaraphael65@gmail.com>
2023-04-23 09:21:41 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry Point
- * Description: 'print alphabets in lowercase except q and e'
+ * main - A program that print alphabet in lowercase
  * Return: Always 0
  */
 int main(void)
 {
-	int n = 97;
+int n = 97;
 
-	while (n <= 122)
-	{
-		if (n == 101 || n == 113)
-		{
-			n++;
-			continue;
-                }
-		putchar(n);
-		n++;
-		  	
-	}	
-	putchar('\n');
-	return (0);
+while (n <= 122)
+{
+if (n == 101 || n == 113)
+{
+n++;
+continue;
+}
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
 }
