9574e3e3632a2333d0918b0127736f4702399cd6
AugustaOrji <augustaraphael65@gmail.com>
2023-04-25 05:16:52 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'r'; letter++)
+{
+	_putchar(letter);
+}
+	_putchar('\n');
+}
