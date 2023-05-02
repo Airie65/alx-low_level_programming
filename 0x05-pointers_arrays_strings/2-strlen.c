aec2e6a86aa8d22c2f1944e783cdb0c8f3b8bab0
AugustaOrji <augustaraphael65@gmail.com>
2023-05-02 15:00:02 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,12 +10,10 @@
 int _strlen(char *s)
 {
 
-	int longi = 0;
+	int count = 0;
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-	return (longi);
+	while (s[count] != '\0')
+		count++;
+
+	return (count);
 }
