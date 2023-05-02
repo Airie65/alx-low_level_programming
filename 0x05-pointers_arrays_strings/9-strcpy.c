3da45a45ce088e978b954aad3d387580f23fd003
AugustaOrji <augustaraphael65@gmail.com>
2023-05-02 11:06:01 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _stropy - a  function that copies the string pointed to by src
+ * _strcpy - a  function that copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
  * Return: dest
